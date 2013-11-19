52eca339b2c0fc1c1762786100af08b45ec9bd7f
Pedrovilela <pedro.vilela_94@hotmail.com>
2013-11-19 17:37:57 -0300

    Adicionado os predicados dinamicos basicos.

--- agente004.pl
+++ agente004.pl
@@ -1,5 +1,11 @@
 :- load_files([wumpus1]).
-
+:- use_module(library(lists)).
+:- dynamic([localizacao/2, %determina onde você está.
+     		direcao/1, %Determina para onde você está olhando.
+			lugares_seguros/2, %Lugares que são seguros
+			lugares_inexplorados/2,%Lugares para explorar caso o caminho principal falhou
+			turno/1%contador de turno. O mais importante é o primeiro(fugir) e o 40(desistir de mais ouros)
+		]).
 
 init_agent:-
     writeln('agente inicializado...').
@@ -12,6 +18,3 @@ run_agent(Pe,Ac):-
 
 
 agente004([_,_,_,_,_],goforward).
-
-huehuehuehue
-
