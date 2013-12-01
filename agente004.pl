76e8ac1d251c372a8b5c7cca6bcb44b6322ac881
Pedrovilela <pedro.vilela_94@hotmail.com>
2013-12-01 10:55:22 -0300

    Eliminada a condicao de ouro no primeiro turno, eliminado os espacos no meio de textos, adicionado funcao_atualizador

--- agente004.pl
+++ agente004.pl
@@ -32,36 +32,30 @@ restart_agent:-
     init_agent.
 
 run_agent(Pe,Ac):-
-    pega_ouro(Pe,Ac);
-	tempo_limite(Pe),
-    correndo_tempo(Pe,Ac),
-	turno1(Pe),
-    desistindo(Pe,Ac);
-	
-        atualizador(Pe),
-        verificador(Pe),
-    movimento(Pe,Ac).
+    				pega_ouro(Pe,Ac);
+					tempo_limite(Pe),
+ 					correndo_tempo(Pe,Ac);
+					turno1(Pe),
+    				desistindo(Pe,Ac);
+   					atualizador(Pe),
+					verificador(Pe),
+   					movimento(Pe,Ac).
+
 pega_ouro([_,_,yes,_,_],grab):-
-                                ouro(O),
-                                On is O+1,
-                                retractall(ouro(_)),
-                                assert(ouro(On)),
                                 turno(T),
                                 Tn is T+1,
                                 retractall(turno(_)),
                                 assert(turno(Tn)).
-								turno(1),
-								retractall(ouro(_)),
-								assert(ouro(1)).
 tempo_limite(_):-
-                turno(T),
-                T>54,
-                retractall(modo_fuga(_)),
-                assert(modo_fuga(1)).
+                	turno(T),
+                	T>54,
+                	retractall(modo_fuga(_)),
+                	assert(modo_fuga(1)).
+
 
 correndo_tempo(_,climb):-
-                        modo_fuga(1),
-                        localizacao(1,1).
+                       		modo_fuga(1),
+                       		localizacao(1,1).
 correndo_tempo(_,Ac):-
                        modo_fuga(1),
                        localizacao(X,Y),
@@ -145,86 +139,94 @@ atualizador(_):-
 
 
 movimento(_,Ac):-
-                                localizacao(X,Y),
-                                casa_inexplorada(Lista),
-                                nth0(0,Lista,Proximo),
+                localizacao(X,Y),
+                casa_inexplorada(Lista),
+                nth0(0,Lista,Proximo),
                 proximacasa((X,Y),(A,B),Proximo),
                 !,
                 acao((A,B),Ac).
 
 
-acao((C,_),turnright):-localizacao(X,_),
+acao((C,_),turnright):-
+						localizacao(X,_),
                         X>C,
-                                angulo(3),
+                        angulo(3),
                         retractall(angulo(_)),
                         assert(angulo(2)).
 
-acao((C,_),turnleft):- localizacao(X,_),
+acao((C,_),turnleft):- 
+						localizacao(X,_),
                         X>C,
-                               angulo(I),
+                        angulo(I),
                         I<2,
                         In is I+1,
                         retractall(angulo(_)),
                         assert(angulo(In)).
 
-acao((C,_),turnleft):- localizacao(X,_),
+acao((C,_),turnleft):- 
+						localizacao(X,_),
                         X<C,
-                                    angulo(3),
+                        angulo(3),
                         retractall(angulo(_)),
                         assert(angulo(0)).
 
-acao((C,_),turnright):-localizacao(X,_),
+acao((C,_),turnright):-
+						localizacao(X,_),
                         X<C,
-                            angulo(I),
+                        angulo(I),
                         I>0,
                         In is I-1,
                         retractall(angulo(_)),
                         assert(angulo(In)).
 
-	acao((_,D),turnright):-localizacao(_,Y),
+acao((_,D),turnright):-
+						localizacao(_,Y),
                         Y<D,
-                                angulo(I),
+                        angulo(I),
                         I>1,
                         In is I-1,
                         retractall(angulo(_)),
                         assert(angulo(In)).
 
-	acao((_,D),turnleft):- localizacao(_,Y),
-                        Y<D,
-                               angulo(0),
-                       retractall(angulo(_)),
-                        assert(angulo(1)).
+acao((_,D),turnleft):-
+					 	localizacao(_,Y),
+                     	Y<D,
+                     	angulo(0),
+                     	retractall(angulo(_)),
+                     	assert(angulo(1)).
 
-	acao((_,D),turnright):-  localizacao(_,Y),
-                        Y>D,
-                                angulo(0),
-                        retractall(angulo(_)),
-                        assert(angulo(3)).
+acao((_,D),turnright):-
+					 	localizacao(_,Y),
+                     	Y>D,
+                     	angulo(0),
+                     	retractall(angulo(_)),
+                     	assert(angulo(3)).
 
-	acao((_,D),turnleft):-localizacao(_,Y),
-                        Y>D,
-                                 angulo(I),
-                        I<3,
-                        In is I+1,
-                        retractall(angulo(_)),
-                        assert(angulo(In)).
+acao((_,D),turnleft):-
+					 	localizacao(_,Y),
+                     	Y>D,
+               		 	angulo(I),
+                     	I<3,
+                     	In is I+1,
+                     	retractall(angulo(_)),
+                     	assert(angulo(In)).
 
-	acao((_,_),goforward):-
-                                                atualizador_localizacao,
-                                                localizacao(X,Y),
-                                                casa_inexplorada(Lista),
-                                                delete(Lista,(X,Y),Novalista),
-                                                retractall(casa_inexplorada(_)),
-                                                assert(casa_inexplorada(Novalist                                                                                        a)).
+acao((_,_),goforward):-
+                     	atualizador_localizacao,
+                     	localizacao(X,Y),
+                     	casa_inexplorada(Lista),
+                     	delete(Lista,(X,Y),Novalista),
+                      	retractall(casa_inexplorada(_)),
+                      	assert(casa_inexplorada(Novalista)).
 							
 caminho((A,B),(C,D)):-
-                                         casa_adjacente((A,B),(C,D)),
-                     impede_loop((A,B),(C,D)).
+                     	casa_adjacente((A,B),(C,D)),
+                     	impede_loop((A,B),(C,D)).
 
 caminho((A,B),(C,D)):-
-                     casa_adjacente((A,B),(E,F)),
-                     impede_loop((A,B),(E,F)),
-                     caminho((E,F),(C,D)).
+                     	casa_adjacente((A,B),(E,F)),
+                     	impede_loop((A,B),(E,F)),
+                     	caminho((E,F),(C,D)).
 
 proximacasa((A,B),(C,D),(E,F)):-
                                 casa_adjacente((A,B),(E,F)),
@@ -233,45 +235,63 @@ proximacasa((A,B),(C,D),(E,F)):-
 
 proximacasa((A,B),(C,D),(E,F)):-
                                 casa_adjacente((A,B),(C,D)),
-                                                                retractall(camin                                                                                        hos_ja_passados(_)),
-                                                                assert(caminhos_                                                                                        ja_passados([[(50,50),(50,50)]])),
+                                retractall(caminhos_ja_passados(_)),
+                                assert(caminhos_ja_passados([[(50,50),(50,50)]])),
                                 impede_loop((A,B),(C,D)),
                                 caminho((C,D),(E,F)).
 impede_loop((A,B),(C,D)):-
-                       caminhos_ja_passados(Lista),
-                       intersection([[(A,B),(C,D)]],Lista,[]),
-                       append([[(A,B),(C,D)],[(C,D),(A,B)]],Lista,Novalista),
-                       retractall(caminhos_ja_passados(_)),
-                       assert(caminhos_ja_passados(Novalista)).
+                       		caminhos_ja_passados(Lista),
+                       		intersection([[(A,B),(C,D)]],Lista,[]),
+                       		append([[(A,B),(C,D)],[(C,D),(A,B)]],Lista,Novalista),
+                       		retractall(caminhos_ja_passados(_)),
+                       		assert(caminhos_ja_passados(Novalista)).
 
 
 %Essas funções atualizam a localizacao do Agente dependendo para onde ele está olhando.
 
 atualizador_localizacao:-
-       localizacao(X,Y),
-       angulo(0),
-       Xn is X+1,
-       retractall(localizacao(_,_)),
-       assert(localizacao(Xn,Y)).
+       					localizacao(X,Y),
+       					angulo(0),
+       					Xn is X+1,
+       					retractall(localizacao(_,_)),
+       					assert(localizacao(Xn,Y)).
 
 atualizador_localizacao:-
-       localizacao(X,Y),
-       angulo(1),
-       Yn is Y+1,
-       retractall(localizacao(_,_)),
-       assert(localizacao(X,Yn)).
+       					localizacao(X,Y),
+       					angulo(1),
+       					Yn is Y+1,
+       					retractall(localizacao(_,_)),
+       					assert(localizacao(X,Yn)).
 
 atualizador_localizacao:-
-       localizacao(X,Y),
-       angulo(2),
-       Xn is X-1,
-       retractall(localizacao(_,_)),
-       assert(localizacao(Xn,Y)).
+       					localizacao(X,Y),
+      					angulo(2),
+       					Xn is X-1,
+       					retractall(localizacao(_,_)),
+       					assert(localizacao(Xn,Y)).
 
 atualizador_localizacao:-
-       localizacao(X,Y),
-       angulo(3),
-       Yn is Y-1,
-       retractall(localizacao(_,_)),
-       assert(localizacao(X,Yn)).
+       					localizacao(X,Y),
+       					angulo(3),
+       					Yn is Y-1,
+       					retractall(localizacao(_,_)),
+       					assert(localizacao(X,Yn)).
 
+%essas tres servem para verificar se as casas ao lado existem e nao sao exploradas.
+
+funcao_atualizador((_,_),(A,B)):-
+                               casa_segura(A,B).
+
+funcao_atualizador((X,Y),(A,B)):-
+            		            A>0,
+                    		    A<5,
+                    		    B>0,
+                    		    B<5,
+                                assert(casa_segura(A,B)),
+                                casa_inexplorada(Lista),
+                                append([(A,B)],Lista,Novalista),
+                                retractall(casa_inexplorada(_)),
+                                assert(casa_inexplorada(Novalista)),
+                                assert(casa_adjacente((X,Y),(A,B))),
+                                assert(casa_adjacente((A,B),(X,Y))).
+funcao_atualizador((_,_),(_,_)).
